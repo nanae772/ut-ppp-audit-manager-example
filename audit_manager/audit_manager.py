b5f7508b1c2e8049432da7bb4b749e7d2cc179b4
nanae <desperado.kazune@gmail.com>
2025-01-03 12:38:38 +0900

    Refactor AuditManager to use FileContent and FileUpdate classes; update tests accordingly

--- audit_manager/audit_manager.py
+++ audit_manager/audit_manager.py
@@ -1,84 +1,101 @@
-from abc import ABC, abstractmethod
 from pathlib import Path
 import re
 
 
-class IFileSystem(ABC):
-    @abstractmethod
-    def get_files(directory_name: str) -> list[str]:
-        pass
-
-    @abstractmethod
-    def write_all_text(file_path: str, content: str) -> None:
-        pass
-
-    @abstractmethod
-    def read_all_lines(file_path: str) -> list[str]:
-        pass
+class FileContent:
+    def __init__(self, file_name: str, lines: list[str]):
+        self.file_name: str = file_name
+        self.lines: list[str] = lines
 
 
-class FileSystem(IFileSystem):
-    @staticmethod
-    def get_files(directory_name: str) -> list[str]:
-        directory_path = Path(f"./{directory_name}")
-        if not directory_path.is_dir():
-            raise Exception(f"{directory_path} doesn't exists or is not directory.")
+class FileUpdate:
+    def __init__(self, file_name: str, new_content: str):
+        self.file_name: str = file_name
+        self.new_content: str = new_content
 
-        return sorted(str(item) for item in directory_path.iterdir() if item.is_file())
-
-    @staticmethod
-    def write_all_text(file_path: str, content: str) -> None:
-        Path(file_path).write_text(content)
-
-    @staticmethod
-    def read_all_lines(file_path: str) -> list[str]:
-        return Path(file_path).read_text().splitlines()
+    def __eq__(self, other: "FileUpdate") -> bool:
+        return (
+            self.file_name == other.file_name and self.new_content == other.new_content
+        )
 
 
 class AuditManager:
     """訪問者記録システム"""
 
-    def __init__(
-        self, max_entries_per_file: int, directory_name: str, file_system: IFileSystem
-    ) -> "AuditManager":
+    def __init__(self, max_entries_per_file: int) -> "AuditManager":
         self._max_entries_per_file = max_entries_per_file
-        self._directory_name = directory_name
-        self._file_system = file_system
 
-    def add_record(self, visitor_name: str, time_of_visit: str) -> None:
+    def add_record(
+        self, files: list[FileContent], visitor_name: str, time_of_visit: str
+    ) -> FileUpdate:
         """訪問者の名前と訪問時刻をファイルに記録する"""
-        file_paths = self._file_system.get_files(self._directory_name)
 
-        new_record = f"{visitor_name};{time_of_visit}"
+        def sort_by_index(files: list[FileContent]) -> list[(int, FileContent)]:
+            def get_index(file_name: str) -> int:
+                pattern = r"audit_(\d)+\.txt"
+                return int(re.search(pattern, file_name).group(1))
 
-        # ディレクトリが空ならaudit_1.txtを新しく作成し、そこに記録
-        if len(file_paths) == 0:
-            new_file_path = f"./{self._directory_name}/audit_1.txt"
-            self._file_system.write_all_text(new_file_path, new_record)
-            return
+            return sorted((get_index(file.file_name), file) for file in files)
 
-        current_file_path = file_paths[-1]  # 最新のファイルパスを取得
-        lines = self._file_system.read_all_lines(current_file_path)
+        sorted_files: list[(int, FileContent)] = sort_by_index(files)
+        new_record: str = f"{visitor_name};{time_of_visit}"
+
+        if len(sorted_files) == 0:
+            return FileUpdate("audit_1.txt", new_record)
+
+        current_file_index: int
+        current_file: FileContent
+        current_file_index, current_file = sorted_files[-1]
+        lines: list[str] = current_file.lines
 
         if len(lines) < self._max_entries_per_file:
             lines.append(new_record)
-            new_content = "\n".join(lines)
-            self._file_system.write_all_text(current_file_path, new_content)
+            new_content: str = "\n".join(lines)
+            return FileUpdate(current_file.file_name, new_content)
         else:
-            # 最新のファイルが最大行に達していたなら、新規ファイルを作成しそこに記録
-            pattern = r"audit_(\d+)\.txt"
-            match = re.search(pattern, current_file_path)
-            current_file_index = int(match.group(1))
-            new_index = current_file_index + 1
+            new_index: int = current_file_index + 1
+            new_name: str = f"audit_{new_index}.txt"
+            return FileUpdate(new_name, new_record)
 
-            new_name = f"audit_{new_index}.txt"
-            new_file_path = f"./{self._directory_name}/{new_name}"
-            self._file_system.write_all_text(new_file_path, new_record)
+
+class Persister:
+    """訪問者記録ログの永続化に関する処理を担当するクラス"""
+
+    @staticmethod
+    def read_directory(directory_name: str) -> list[FileContent]:
+        """指定したdirectoryを読み込んでファイルのリストを返す"""
+        return [
+            FileContent(file.name, file.read_text().splitlines())
+            for file in Path(directory_name).iterdir()
+            if file.is_file()
+        ]
+
+    @staticmethod
+    def apply_update(directory_name: str, update: FileUpdate) -> None:
+        """FileUpdateの内容に基づいてファイルの新規作成および更新を行う"""
+        Path(directory_name).joinpath(update.file_name).write_text(update.new_content)
+
+
+class ApplicationService:
+    """AuditManagerとPersisterの連携を指揮するクラス"""
+
+    def __init__(self, directory_name: str, max_entries_per_file: int):
+        self._directory_name: str = directory_name
+        self._audit_manager: AuditManager = AuditManager(max_entries_per_file)
+        self._persister: Persister = Persister
+
+    def add_record(self, visitor_name: str, time_of_visit: str) -> None:
+        update: FileUpdate = self._audit_manager.add_record(
+            self._persister.read_directory(self._directory_name),
+            visitor_name,
+            time_of_visit,
+        )
+        self._persister.apply_update(self._directory_name, update)
 
 
 if __name__ == "__main__":
     import sys
 
-    audit_manager = AuditManager(3, "audit_logs", FileSystem)
+    application_service = ApplicationService("audit_logs", 3)
     visitor_name, time_of_visit = sys.argv[1:]
-    audit_manager.add_record(visitor_name, time_of_visit)
+    application_service.add_record(visitor_name, time_of_visit)
--- tests/test_audit_manager.py
+++ tests/test_audit_manager.py
@@ -1,5 +1,4 @@
-from audit_manager.audit_manager import AuditManager, IFileSystem
-from unittest.mock import MagicMock
+from audit_manager.audit_manager import AuditManager, FileContent, FileUpdate
 
 
 class TestAuditManager:
@@ -12,22 +11,18 @@ class TestAuditManager:
     def test_create_init_file(self):
         """ディレクトリが空のとき、audit_1.txtを作成しそこにログを記録"""
         # Arrange
-        file_system_mock = MagicMock(name="file_system_mock", spec=IFileSystem)
-        file_system_mock.get_files.return_value = []  # 空のディレクトリ
-
-        sut = AuditManager(
-            TestAuditManager.max_entries_per_file,
-            TestAuditManager.directory_name,
-            file_system_mock,
-        )
+        files: list[FileContent] = []
+        sut = AuditManager(TestAuditManager.max_entries_per_file)
 
         # Act
-        sut.add_record(TestAuditManager.visitor_name, TestAuditManager.time_of_visit)
+        update = sut.add_record(
+            files, TestAuditManager.visitor_name, TestAuditManager.time_of_visit
+        )
 
         # Assert
         # 新しいファイルにレコードが書き込まれることを確認
-        file_system_mock.write_all_text.assert_called_once_with(
-            f"./{TestAuditManager.directory_name}/audit_1.txt",
+        assert update == FileUpdate(
+            "audit_1.txt",
             f"{TestAuditManager.visitor_name};{TestAuditManager.time_of_visit}",
         )
 
@@ -36,55 +31,43 @@ class TestAuditManager:
         ディレクトリに既にファイルが存在し、最新ファイルが最大行に達していないなら最新ファイルにログを記録
         """
         # Arrange
-        file_system_mock = MagicMock(name="file_system_mock", spec=IFileSystem)
-
-        existing_file_path = f"./{TestAuditManager.directory_name}/audit_1.txt"
-        file_system_mock.get_files.return_value = [existing_file_path]
-
-        existing_file_content = "Alice;2024-12-28T21:00:00Z"
-        file_system_mock.read_all_lines.return_value = [existing_file_content]
-
-        sut = AuditManager(
-            TestAuditManager.max_entries_per_file,
-            TestAuditManager.directory_name,
-            file_system_mock,
-        )
+        files: list[FileContent] = [
+            FileContent("audit_1.txt", ["Alice;2024-12-28T21:00:00Z"]),
+        ]
+        sut = AuditManager(TestAuditManager.max_entries_per_file)
 
         # Act
-        sut.add_record(TestAuditManager.visitor_name, TestAuditManager.time_of_visit)
+        update = sut.add_record(
+            files, TestAuditManager.visitor_name, TestAuditManager.time_of_visit
+        )
 
         # Assert
         # 既にあるファイルにレコードが書き込まれることを確認
-        file_system_mock.write_all_text.assert_called_once_with(
-            existing_file_path,
-            f"{existing_file_content}\n{TestAuditManager.visitor_name};{TestAuditManager.time_of_visit}",
+        assert update == FileUpdate(
+            "audit_1.txt",
+            f"Alice;2024-12-28T21:00:00Z\n{TestAuditManager.visitor_name};{TestAuditManager.time_of_visit}",
         )
 
     def test_create_new_file_when_latest_is_max(self):
         """最新ファイルが最大行に達しているなら、新規のファイルを作成しそこにログを記録"""
         # Arrange
-        file_system_mock = MagicMock(name="file_system_mock", spec=IFileSystem)
-
-        file_system_mock.get_files.return_value = [
-            f"./{TestAuditManager.directory_name}/audit_1.txt"
+        files: list[FileContent] = [
+            FileContent("audit_1.txt", ["Alice;2024-12-28T21:00:00Z"]),
+            FileContent(
+                "audit_2.txt",
+                ["Alice;2024-12-28T21:00:00Z"] * TestAuditManager.max_entries_per_file,
+            ),
         ]
-
-        file_system_mock.read_all_lines.return_value = [
-            "Alice;2024-12-28T21:00:00Z"
-        ] * TestAuditManager.max_entries_per_file
-
-        sut = AuditManager(
-            TestAuditManager.max_entries_per_file,
-            TestAuditManager.directory_name,
-            file_system_mock,
-        )
+        sut = AuditManager(TestAuditManager.max_entries_per_file)
 
         # Act
-        sut.add_record(TestAuditManager.visitor_name, TestAuditManager.time_of_visit)
+        update = sut.add_record(
+            files, TestAuditManager.visitor_name, TestAuditManager.time_of_visit
+        )
 
         # Assert
         # 新しいファイルにレコードが書き込まれることを確認
-        file_system_mock.write_all_text.assert_called_once_with(
-            f"./{TestAuditManager.directory_name}/audit_2.txt",
+        assert update == FileUpdate(
+            "audit_3.txt",
             f"{TestAuditManager.visitor_name};{TestAuditManager.time_of_visit}",
         )
